1389e0fedc7bbf0c6cfc989756c7f4f2d4cc0a3e
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-04-12 18:52:30 -0400

    finished api implementation

--- checkmypass.py
+++ checkmypass.py
@@ -8,8 +8,19 @@ def request_api_data(query_char):
     raise RuntimeError(f'Error communicating with API {res.status_code}')
   return res
 
+def get_password_leaks_count(hashes, hash_to_check):
+  hashes= (line.split(':') for line in hashes.text.splitlines())
+  for h, count in hashes:
+    if h == hash_to_check:
+      return count
+  return 0
+
 def pwned_api_check(password):
-  return (hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
+  sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+  first5_char, tail = sha1password[:5], sha1password[5:]
+  response = request_api_data(first5_char)
+  response2 = get_password_leaks_count(response,tail)
+  return response2
 
 
 print(pwned_api_check('123'))
